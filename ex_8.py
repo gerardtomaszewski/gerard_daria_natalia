40c1528cc493fd46370cbb63a0ca451cc9007c96
Gerard Tomaszewski <tomaszewskigerard@gmail.com>
2023-04-24 12:27:25 +0200

    add whitespace

--- ex_8.py
+++ ex_8.py
@@ -3,6 +3,7 @@
 # dla malejacej zwroc False
 
 def funkcja_liniowa(punkty) -> bool:
+    
     posortowane_punkty = sorted(punkty, key=lambda x: x[0])
     y1=posortowane_punkty[0][1]
     y2=posortowane_punkty[1][1]
