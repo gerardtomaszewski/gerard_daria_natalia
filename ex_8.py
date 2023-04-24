33a83992193e075e45d7c0ff2963ebf7362645c8
Gerard Tomaszewski <tomaszewskigerard@gmail.com>
2023-04-24 12:36:52 +0200

    fix

--- ex_8.py
+++ ex_8.py
@@ -4,11 +4,11 @@
 
 def funkcja_liniowa(punkty) -> bool:
     
-    posortowane_punkty = sorted(punkty, key=lambda x: x[0])
-    y1=posortowane_punkty[0][1]
-    y2=posortowane_punkty[1][1]
+    sorted(punkty, key=lambda x: x[0])
+    y1=punkty[0][1]
+    y2=punkty[1][1]
     
-    if y2>y1:
+    if y2>=y1:
         return True
     else:
         return False
